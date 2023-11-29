37b4c970b0f90bf2f95d64626b2da48b8268a050
kkateq <kateryna.musina@gmail.com>
2023-11-29 12:05:03 +0100

    Added slider.

--- frontend/app/components/orderform.tsx
+++ frontend/app/components/orderform.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { Order, OrderType } from "./commons";
+import { Slider } from "@material-tailwind/react";
 
 type Props = {
   orderAmount: number;
@@ -14,7 +15,7 @@ export const OrderForm = ({
   onChangeScaleInOut,
 }: Props) => {
   const [orderType, setOrderType] = useState<OrderType>();
-  const [simpleForm, setSimpleForm] = useState(true);
+  const [simpleForm, setSimpleForm] = useState<boolean>(true);
 
   const handleOrderTypeSelection = (
     e: React.ChangeEvent<HTMLSelectElement>
@@ -30,6 +31,10 @@ export const OrderForm = ({
     onChangeScaleInOut(!scaleInOut);
   };
 
+  const handleSimpleForm = () => {
+    setSimpleForm(!simpleForm);
+  };
+
   const handleBuy = () => {
     console.log("buy");
   };
@@ -42,6 +47,11 @@ export const OrderForm = ({
   const handleFlatten = () => {
     console.log("flatten");
   };
+
+  const handleVolumeChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    onChangeOrderAmount(e.target.value as any);
+  };
+
   return (
     <div className="mt-2 border-solid border-2 border-gray-400 rounded p-2 bg-white">
       <h4 className="mb-2 text-gray-500">Order settings</h4>
@@ -54,48 +64,63 @@ export const OrderForm = ({
           onChange={handleAmountChange}
           className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 p-2"
         ></input>
-        <select
-          id="orderTypeSelect"
-          onChange={handleOrderTypeSelection}
-          value={orderType}
-          className="p-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
-        >
-          <option value={Order.limit}>limit</option>
-          <option value={Order.market}>market</option>
-          <option value={Order.stop}>stop</option>
-        </select>
 
-        <div className="relative flex gap-x-3">
-          <div className="flex h-6 items-center">
-            <input
-              type="checkbox"
-              name="scale"
-              id="scale"
-              checked={scaleInOut}
-              onChange={handleScaleInOutChange}
-              className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
-            ></input>
-          </div>
-          <div className="text-sm leading-6">
-            <label htmlFor="scale" className="font-medium text-gray-900">
-              Scale in/out
-            </label>
-          </div>
+        <div className="pb-6 flex">
+          <Slider
+            color="green"
+            size="md"
+            defaultValue={50}
+            onChange={handleVolumeChange}
+            step={0.001}
+          />
         </div>
-        <button
-          type="button"
-          onClick={handleBuy}
-          className="rounded-md bg-sky-600 px-3 py-1 text-sm font-semibold text-white shadow-sm hover:bg-sky-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-        >
-          BUY
-        </button>
-        <button
-          type="button"
-          onClick={handleSell}
-          className="rounded-md bg-pink-600 px-3 py-1 text-sm font-semibold text-white shadow-sm hover:bg-pink-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-        >
-          SELL
-        </button>
+
+        {!simpleForm && (
+          <>
+            <select
+              id="orderTypeSelect"
+              onChange={handleOrderTypeSelection}
+              value={orderType}
+              className="p-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
+            >
+              <option value={Order.limit}>limit</option>
+              <option value={Order.market}>market</option>
+              <option value={Order.stop}>stop</option>
+            </select>
+
+            <div className="relative flex gap-x-3">
+              <div className="flex h-6 items-center">
+                <input
+                  type="checkbox"
+                  name="scale"
+                  id="scale"
+                  checked={scaleInOut}
+                  onChange={handleScaleInOutChange}
+                  className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
+                ></input>
+              </div>
+              <div className="text-sm leading-6">
+                <label htmlFor="scale" className="font-medium text-gray-900">
+                  Scale in/out
+                </label>
+              </div>
+            </div>
+            <button
+              type="button"
+              onClick={handleBuy}
+              className="rounded-md bg-sky-600 px-3 py-1 text-sm font-semibold text-white shadow-sm hover:bg-sky-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              BUY
+            </button>
+            <button
+              type="button"
+              onClick={handleSell}
+              className="rounded-md bg-pink-600 px-3 py-1 text-sm font-semibold text-white shadow-sm hover:bg-pink-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              SELL
+            </button>
+          </>
+        )}
         <button
           type="button"
           onClick={handleCancelAll}
@@ -110,6 +135,24 @@ export const OrderForm = ({
         >
           FLATTEN
         </button>
+
+        <div className="relative flex gap-x-3">
+          <div className="flex h-6 items-center">
+            <input
+              type="checkbox"
+              name="simpleForm"
+              id="simpleForm"
+              checked={simpleForm}
+              onChange={handleSimpleForm}
+              className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
+            ></input>
+          </div>
+          <div className="text-sm leading-6">
+            <label htmlFor="simpleForm" className="font-medium text-gray-900">
+              Simple form
+            </label>
+          </div>
+        </div>
       </div>
     </div>
   );
